d2391ea44304793c1af4f7e408e2dc914315bc2d
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2026-04-26 20:50:14 +0300

    types: extend next-auth types with user role

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,6 +5,7 @@ declare module "next-auth" {
     user: {
       id: string
       email: string
+      role?: string   
       name?: string | null
       image?: string | null
     }
@@ -15,5 +16,6 @@ declare module "next-auth/jwt" {
   interface JWT {
     id: string
     email: string
+    role?: string   
   }
 }
