f0eadc40091c12953b190dbcc1b80ceb76f0543a
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2026-05-14 22:12:17 +0300

    Configure Jest coverage settings

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,35 @@ const createJestConfig = nextJest({
 })
 
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
+  // Підключення jest setup
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.tsx",
+  ],
+
+  // Середовище браузера
   testEnvironment: "jsdom",
+
+  // Увімкнення coverage
+  collectCoverage: true,
+
+  // Які файли враховувати
+  collectCoverageFrom: [
+
+    // Компоненти, які тестуються
+    "src/components/Footer.tsx",
+    "src/components/Navbar.tsx",
+    "src/components/ProductImage.tsx",
+    "src/components/ProductSlider.tsx",
+    "src/components/ReviewForm.tsx",
+
+    // Не враховувати test файли
+    "!**/*.test.{ts,tsx}",
+
+    // Не враховувати node_modules
+    "!**/node_modules/**",
+  ],
 }
 
-module.exports = createJestConfig(customJestConfig)
+module.exports =
+  createJestConfig(customJestConfig)
