6b73031cfccee4a3b77ac52d2c701cb346053d5f
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2026-05-17 20:55:05 +0300

    Implement component testing with Jest and React Testing Library

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -1,21 +1,53 @@
 import "@testing-library/jest-dom"
 
+// fetch mock
 global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () => Promise.resolve({}),
   })
 ) as jest.Mock
 
+// next/image mock
 jest.mock("next/image", () => ({
   __esModule: true,
 
-  default: (props: unknown) => {
-
-    return (
-      <img
-        alt=""
-        {...(props as object)}
-      />
-    )
+  default: ({
+    fill,
+    priority,
+    ...props
+  }: any) => {
+    return <img {...props} />
   },
+}))
+
+// next/navigation mock
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      refresh: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+    }
+  },
+}))
+
+// next-auth mock
+jest.mock("next-auth/react", () => ({
+  __esModule: true,
+
+  useSession: jest.fn(() => ({
+    data: null,
+    status: "unauthenticated",
+  })),
+
+  signIn: jest.fn(),
+
+  signOut: jest.fn(),
+
+  SessionProvider: ({
+    children,
+  }: {
+    children: React.ReactNode
+  }) => children,
 }))
