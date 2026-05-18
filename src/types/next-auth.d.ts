a6b8f133d5e41f820d596cc98d679aa2d812ad38
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2026-05-18 12:48:56 +0300

    Оновлення

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,29 +1,39 @@
+// Імпортуємо тип стандартної сесії з next-auth
 import type {
   DefaultSession,
 } from "next-auth"
 
+// Розширення типів для next-auth
 declare module "next-auth" {
 
+  // Описуємо власну структуру Session
   interface Session {
 
     user: {
 
+      // Унікальний ID користувача
       id: string
 
+      // Роль користувача (USER / ADMIN)
       role?: string
 
+      // Додаємо стандартні поля user (name, email, image)
     } & DefaultSession["user"]
   }
 }
 
+// Розширення типів JWT токена
 declare module "next-auth/jwt" {
 
   interface JWT {
 
+    // ID користувача в токені
     id?: string
 
+    // Роль користувача
     role?: string
 
+    // Фото профілю користувача
     picture?: string | null
   }
 }
